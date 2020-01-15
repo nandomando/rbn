81342f7f0372b9bf5299b7469ea9050baa94bdd2
Fernando Orozco <fernando.orozco@hotmail.fr>
2020-01-15 22:16:44 +0100

    dark mode

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './new-offer.page.html',
   styleUrls: ['./new-offer.page.scss'],
 })
+
 export class NewOfferPage implements OnInit {
 
   constructor() { }
