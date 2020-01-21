90a662e043c525e0021dda768e759b3bae9a00bf
Fernando Orozco <fernando.orozco@hotmail.fr>
2020-01-21 15:05:43 +0100

    template form

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
   selector: 'app-auth',
@@ -33,5 +35,9 @@ export class AuthPage implements OnInit {
         this.router.navigateByUrl('/places/tabs/discover');
       }, 1500);
     });
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 }
