c1173e73e6be1a6070eb9f5032fe3f1dc5fb3479
Fernando Orozco <fernando.orozco@hotmail.fr>
2020-01-23 16:04:28 +0100

    random dates code

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -11,21 +11,27 @@ export class PlacesService {
       'Manhattan Mansion',
       'In the heart of New York',
       'https://images.pexels.com/photos/87378/pexels-photo-87378.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
-      399.99
+      399.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
     ),
     new Place(
       'p2',
       'White House',
       'In AL',
       'https://images.pexels.com/photos/1115804/pexels-photo-1115804.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
-      150.00
+      150.00,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
     ),
     new Place(
       'p3',
       'Casa de Papel',
       'In Barcelona',
       'https://images.pexels.com/photos/1268871/pexels-photo-1268871.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
-      280.00
+      280.00,
+      new Date('2019-01-01'),
+      new Date('2019-12-31'),
     )
   ];
 
