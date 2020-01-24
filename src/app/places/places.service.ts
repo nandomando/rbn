fddb6064ec057fadfd5169d1bdfd2c615ce7e724
Fernando Orozco <fernando.orozco@hotmail.fr>
2020-01-24 16:17:14 +0100

    getting data with objs

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Place } from './place.model';
+import { AuthService } from '../auth/auth.service';
+import { BehaviorSubject } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
-  private _places: Place[] = [
+  private _places = new BehaviorSubject<Place[]>([
     new Place(
       'p1',
       'Manhattan Mansion',
@@ -13,7 +16,8 @@ export class PlacesService {
       'https://images.pexels.com/photos/87378/pexels-photo-87378.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
       399.99,
       new Date('2019-01-01'),
-      new Date('2019-12-31'),
+      new Date('2024-12-31'),
+      'abc'
     ),
     new Place(
       'p2',
@@ -22,7 +26,8 @@ export class PlacesService {
       'https://images.pexels.com/photos/1115804/pexels-photo-1115804.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
       150.00,
       new Date('2019-01-01'),
-      new Date('2019-12-31'),
+      new Date('2024-12-31'),
+      'abc'
     ),
     new Place(
       'p3',
@@ -31,16 +36,34 @@ export class PlacesService {
       'https://images.pexels.com/photos/1268871/pexels-photo-1268871.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
       280.00,
       new Date('2019-01-01'),
-      new Date('2019-12-31'),
+      new Date('2024-12-31'),
+      'abc'
     )
-  ];
+  ]
+);
 
   get places() {
-    return [...this._places];
+    return [this._places.asObservable()];
   }
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   getPlace(id: string) {
     return {...this._places.find(p => p.id === id)};
+  }
+
+  addPlace(title: string, description: string, price: number, dateFrom: Date, dateTo: Date) {
+    const newPlace = new Place(
+      Math.random().toString(),
+      title,
+      description,
+      'https://images.pexels.com/photos/1268871/pexels-photo-1268871.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 
+      price,
+      dateFrom,
+      dateTo,
+      this.authService.userId
+    );
+    this._places.pipe(take(1)).subscribe(places => {
+      this._places.next(places.concat(newPlace));
+    });
   }
 }
