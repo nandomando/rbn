67fe112cc96db735e27aa295155a2a8f0d8b5e50
Fernando Orozco <fernando.orozco@hotmail.fr>
2020-01-28 10:50:10 +0100

    conecting to firebase

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -3,6 +3,7 @@ import { Place } from './place.model';
 import { AuthService } from '../auth/auth.service';
 import { BehaviorSubject } from 'rxjs';
 import { take, map, tap, delay } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -44,7 +45,7 @@ export class PlacesService {
   get places() {
     return this._places.asObservable();
   }
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private http: HttpClient) { }
 
   getPlace(id: string) {
     return this.places.pipe(
@@ -66,13 +67,21 @@ export class PlacesService {
       dateTo,
       this.authService.userId
     );
-    return this.places.pipe(
-      take(1),
-      delay(1000),
-      tap(places => {
-        this._places.next(places.concat(newPlace));
+    return this.http
+      .post('https://ion-rbn.firebaseio.com/offered-places.json', { 
+        ...newPlace,
+        id: null
       })
-    );
+      .pipe(tap(resData => {
+        console.log(resData);
+      }));
+    // return this.places.pipe(
+    //   take(1),
+    //   delay(1000),
+    //   tap(places => {
+    //     this._places.next(places.concat(newPlace));
+    //   })
+    // );
   }
 
   updatePlace(placeId: string, title: string, description: string) {
