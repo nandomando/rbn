89dfbbfb1baf813593075c6f109357bc6e19624d
Fernando Orozco <fernando.orozco@hotmail.fr>
2020-01-28 15:13:23 +0100

    fetching data from firebase

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -5,47 +5,90 @@ import { BehaviorSubject } from 'rxjs';
 import { take, map, tap, delay, switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 
+
+interface PlaceData {
+  availableFrom: string;
+  availableTo: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+  title: string;
+  userId: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
-  private _places = new BehaviorSubject<Place[]>([
-    new Place(
-      'p1',
-      'Manhattan Mansion',
-      'In the heart of New York',
-      'https://images.pexels.com/photos/87378/pexels-photo-87378.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
-      399.99,
-      new Date('2019-01-01'),
-      new Date('2024-12-31'),
-      'abc'
-    ),
-    new Place(
-      'p2',
-      'White House',
-      'In AL',
-      'https://images.pexels.com/photos/1115804/pexels-photo-1115804.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
-      150.00,
-      new Date('2019-01-01'),
-      new Date('2024-12-31'),
-      'ac'
-    ),
-    new Place(
-      'p3',
-      'Casa de Papel',
-      'In Barcelona',
-      'https://images.pexels.com/photos/1268871/pexels-photo-1268871.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
-      280.00,
-      new Date('2019-01-01'),
-      new Date('2024-12-31'),
-      'abc'
-    )
-  ]);
+   private _places = new BehaviorSubject<Place[]>([]);
+  //   new Place(
+  //     'p1',
+  //     'Manhattan Mansion',
+  //     'In the heart of New York',
+  //     'https://images.pexels.com/photos/87378/pexels-photo-87378.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+  //     399.99,
+  //     new Date('2019-01-01'),
+  //     new Date('2024-12-31'),
+  //     'abc'
+  //   ),
+  //   new Place(
+  //     'p2',
+  //     'White House',
+  //     'In AL',
+  //     'https://images.pexels.com/photos/1115804/pexels-photo-1115804.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+  //     150.00,
+  //     new Date('2019-01-01'),
+  //     new Date('2024-12-31'),
+  //     'ac'
+  //   ),
+  //   new Place(
+  //     'p3',
+  //     'Casa de Papel',
+  //     'In Barcelona',
+  //     'https://images.pexels.com/photos/1268871/pexels-photo-1268871.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+  //     280.00,
+  //     new Date('2019-01-01'),
+  //     new Date('2024-12-31'),
+  //     'abc'
+  //   )
+
 
   get places() {
     return this._places.asObservable();
   }
   constructor(private authService: AuthService, private http: HttpClient) { }
+
+  fetchPlaces() {
+    return this.http
+      .get<{ [key: string]: PlaceData }>(
+        'https://ion-rbn.firebaseio.com/offered-places.json'
+      )
+      .pipe(map(resData => {
+        const places = [];
+        for (const key in resData) {
+          if (resData.hasOwnProperty(key)) {
+            places.push(
+              new Place(
+
+              key,
+              resData[key].title,
+              resData[key].description,
+              resData[key].imageUrl,
+              resData[key].price,
+              new Date(resData[key].availableFrom),
+              new Date(resData[key].availableTo),
+              resData[key].userId
+              )
+            );
+          }
+        }
+        return places;
+      }),
+      tap(places => {
+        this._places.next(places);
+      })
+    );
+  }
 
   getPlace(id: string) {
     return this.places.pipe(
